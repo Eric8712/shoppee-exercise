53b305909c9e59c39252f895464c2fec8f948253
Eric <t40933777@gmail.com>
2022-07-21 16:53:32 +0800

    sparate pages and setup router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import HomePage from './Pages/HomePage';
+import ProductPage from "./Pages/ProductPage";
+import ProductCollectionPage from './Pages/ProductCollectionPage';
+import CartPage from './Pages/CartPages'
+import CheckoutPage from './Pages/CheckOutPage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  BrowserRouter,
+  Redirect,
+} from "react-router-dom";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Switch>
+   <Route path='/' exact>
+    {/* 返回首頁 */}
+      <Redirect to='/mall'/>
+    </Route>
+    <Route path='/mall' exact>
+      <HomePage />
+    </Route>
+    <Route path='/cart' exact>
+      <CartPage />
+    </Route>
+    <Route path='/checkout' exact>
+      <CheckoutPage />
+    </Route>
+    <Route path='/mall/:categoryName'>
+      <ProductCollectionPage />
+    </Route>
+    <Route path='/:productName'>
+      <ProductPage />
+    </Route>
+   </Switch>
+   </BrowserRouter>
   );
 }
 
